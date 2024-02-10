4e510f891b5eb3ed0f9e6b737651ae9f988b264e
luizcaboclo <luizcaboclo2013@gmail.com>
2024-02-10 17:44:43 -0300

    santa marta

---
+++ my-app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import './style/style.css';
+
+import Logo from './img/logo3'
+
+function App() {
+  return (
+    <>
+      <header>
+          
+         <h1 class="wrap">
+            <button class="button"></button>
+         </h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
